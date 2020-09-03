f09f667ec6dfe64b3e610ab012a2282e3f7902a4
elj97 <1020194@student.sae.edu.au>
2020-09-03 22:44:27 +0800

    pp

---
+++ GAD172/Assets/Scripts/Footsteps.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    CharacterController cc;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if(cc.isGrounded == true && cc.velocity.magnitude > 2f && GetComponent<AudioSource>().isPlaying == false)
+        {
+
+
+            //audioSource.volume = Random.Range(0.8f, 1f);
+            //audioSource.pitch = Random.Range(0.8f, 1.1f);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ GAD172/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 0;
+    private float gravity = 3;
+
+    private CharacterController controller;
+
+    private Vector3 moveDirection = Vector3.zero;
+        
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if(controller.isGrounded)
+        {
+            float moveX = Input.GetAxis("Horizontal");
+            float moveZ = Input.GetAxis("Vertical");
+
+            moveDirection = new Vector3(moveX, 0, moveZ);
+            moveDirection *= moveSpeed;
+
+            
+        }
+
+        moveDirection.y -= gravity;
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    
+}
