2278bc3083395050cd82641c6113d0be31934b50
elj97 <1020194@student.sae.edu.au>
2020-09-05 04:28:54 +0800

    lockd

---
+++ GAD172/Assets/Scripts/Torch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+    public GameObject Light;
+    public bool LightActive;
+    void Start()
+    {
+        Light.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            LightActive = !LightActive;
+        }
+
+        if(LightActive)
+        {
+            TorchlightActive();
+        }
+
+        if(!LightActive)
+        {
+            TorchlightInactive();
+        }
+    }
+
+    void TorchlightActive()
+    {
+
+        Light.SetActive(true);
+    }
+
+    void TorchlightInactive()
+    {
+        Light.SetActive(false);
+    }
+}
